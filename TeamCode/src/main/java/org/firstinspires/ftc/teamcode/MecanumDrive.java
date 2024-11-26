535631d23b45fc893c4e884c3ff0a4c59eb61171
tommasoshyti16@gmail.com <Tommaso16!@>
2024-11-26 17:16:13 -0500

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -76,7 +76,7 @@ public final class MecanumDrive {
         public double maxWheelVel = 50;
         public double minProfileAccel = -30;
         public double maxProfileAccel = 50;
-
+// test
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
         public double maxAngAccel = Math.PI;
